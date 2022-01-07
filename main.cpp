36f0be6c09333ac367beb3a15ee35a21381b97cf
John Galt <John Galt@WIN-M0GNAQUEL9H>
2022-01-07 22:06:20 +0200

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <random>
+
+
+std::string passwordGenerator() {
+	std::string str{ "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#%^&*()_+=-/1234567890!@#%^&*()_+=-/" };
+	std::random_device rd;
+	std::mt19937 generator(rd());
+	std::shuffle(str.begin(), str.end(), generator);
+	return str.substr(0, 16);
+}
+int main() {
+	std::fstream fs;
+	fs.open("C:/Users/John Galt/Desktop/passwords.txt", std::fstream::in | std::fstream::out | std::fstream::app);
+	std::string domain;
+	std::cout << "Enter domain name: (q to exit): ";
+	std::cin >> domain;
+	std::string password = passwordGenerator();
+	std::cout << "\nYou created passord for domain " << domain << "!" << "\n" << domain << '\t' << "| " << "password: " << password << std::endl;
+	fs << "\n" << domain << '\t' << "| " << password;
+	system("pause");
+	return 0;
+}
+
